b442723a5c8fab2c979798a7376d3538c31fd55e
lorensocute <minhthupunki@gmail.com>
2026-08-20 10:37:21 +0700

    Them CorsConfig va ApiResponse chuan chung cho ca nhom

---
+++ src/main/java/Web_Drink_Store/webstore/dto/ApiResponse.java
@@ -0,0 +1,20 @@
+package Web_Drink_Store.webstore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, "OK", data);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+}
